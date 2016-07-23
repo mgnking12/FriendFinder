a2f4b6ccb00709c2794c4b2c0bea3986525ec60d
Megan King <mgnking12@gmail.com>
2016-07-23 14:35:24 -0500

    gitignore

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,26 +1,31 @@
-var friendArr = [];
+var dataFriends = require('../data/friends.js');
 
-app.get('/api/friends', function(req, res) {
+//Exports to the express app
+module.exports = function(app) {
+    app.get('/api/friends', function(req, res) {
+        res.json(dataFriends);
 
+        // for (var i = 0; i < friends.length; i++) {
+        //     res.json(friends[i]);
+        //     return;
+        // }
 
-    for (var i = 0; i < friends.length; i++) {
-        res.json(friends[i]);
-        return;
-    }
+        // res.json(false);
 
-    res.json(false);
+    });
 
-});
+    // Create New Friend - takes in JSON input
+    app.post('/api/friends', function(req, res) {
 
-// Create New Friend - takes in JSON input
-app.post('/api/friends', function(req, res) {
+        // req.body hosts is equal to the JSON post sent from the user
+        var newfriend = req.body;
+        newFriend.friendID = newFriend.friendName.replace(/\s+/g, '').toLowerCase();
+        // We then add the json the user sent to the character array
+        friendArr.push(newfriend);
 
-    // req.body hosts is equal to the JSON post sent from the user
-    var newfriend = req.body;
-    newFriend.friendID = newFriend.friendName.replace(/\s+/g, '').toLowerCase();
-    // We then add the json the user sent to the character array
-    friendArr.push(newfriend);
+        // We then display the JSON to the users
+        res.json(newfriend);
+        // res.json(friendArr);
+    });
 
-    // We then display the JSON to the users
-    res.json(newfriend);
-})
+}
