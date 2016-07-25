852a35935f81bdf285b07c4eab393284122c5a5c
Megan King <mgnking12@gmail.com>
2016-07-25 13:40:24 -0500

    style

--- app/data/friends.js
+++ app/data/friends.js
@@ -9,15 +9,15 @@ var friendArr = [{
 }, {
     name: 'Morgan',
     photo: 'https://scontent-dfw1-1.xx.fbcdn.net/v/t1.0-9/10489949_1443061439296859_2687141935386942844_n.jpg?oh=8af0bafe4383f561df6372fb56b1bf7f&oe=581CADEA',
-    scores: ['5', '1', '2', '5', '5', '2', '5', '3', '1', '3']
+    scores: ['1', '5', '2', '3', '1', '5', '1', '3', '1', '3']
 }, {
     name: 'Sierra',
     photo: 'https://scontent-dfw1-1.xx.fbcdn.net/t31.0-8/12314446_10153329541027098_9040809858596097849_o.jpg',
-    scores: ['5', '1', '2', '5', '5', '2', '5', '3', '1', '3']
+    scores: ['3', '1', '2', '2', '4', '2', '2', '3', '1', '2']
 }, {
     name: 'Noah',
     photo: 'https://scontent-dfw1-1.xx.fbcdn.net/v/t1.0-0/p206x206/11960167_10153319031082098_5302145181025109259_n.jpg?oh=85a3daf6caca440d3e365fb0710624cf&oe=582C75C8',
-    scores: ['5', '1', '2', '5', '5', '2', '5', '3', '1', '3']
+    scores: ['3', '1', '2', '2', '3', '2', '1', '3', '1', '3']
 }];
 
 module.exports = friendArr;
